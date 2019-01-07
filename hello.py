e647db1858f401277317c5b12dfc3e84a1f3c231
Eddie <29959854+gonzales-edwin@users.noreply.github.com>
2019-01-06 19:39:11 -0600

    templates
    all about templates

--- hello.py
+++ hello.py
@@ -1,19 +1,18 @@
-from flask import Flask, request, redirect
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User-Agent')
-    method = request.method
-    ip = request.remote_addr
-    return f'<h2>This is flask. You are using: </h2> {user_agent}<br> {method} from {ip}'
+    return render_template('index.html')
 
 
-@app.route('/reddit/<subreddit>')
-def reddit(subreddit):
-    return redirect(f'https://reddit.com/r/{subreddit}')
+@app.route('/user/<name>')
+def user(name):
+    render_template('user.html', name=name)
 
 
 if __name__ == '__main__':
